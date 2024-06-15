1fc1e6cd3bc6c1ccee626fdd6098b40571aec7a1
rahul508575 <rahul508575@gmail.com>
2024-06-16 01:26:41 +0530

    update patterns

--- DSA/1.js
+++ DSA/1.js
@@ -63,5 +63,96 @@ function pattern5(n) {
         console.log(str);
     }
 }
-
 pattern5(5);
+
+function pattern6(n) {
+    for (let i = 0; i < n; i++) {
+        let ch = '';
+        for (let j = 0; j <= i; j++) {
+            let char = String.fromCharCode('E'.charCodeAt(0) - i + j);
+            ch += char;
+        }
+        console.log(ch);
+    }
+}
+pattern6(5);
+
+function pattern7(n) {
+    for (let i = 0; i < n; i++) {
+        let star = "";
+        for (let j = 0; j < n - i; j++) {
+            star += "*";
+        }
+        for (let j = 0; j < 2 * i; j++) {
+            star += ' ';
+        }
+        for (let j = 0; j < n - i; j++) {
+            star += "*";
+        }
+        console.log(star);
+    }
+
+    for (let i = 1; i <= n; i++) {
+        let star = '';
+        for (let j = 0; j < i; j++) {
+            star += "*";
+        }
+        for (let j = 0; j < 2 * (n - i); j++) {
+            star += ' ';
+        }
+        for (let j = 0; j < i; j++) {
+            star += "*";
+        }
+        console.log(star);
+    }
+}
+
+pattern7(5);
+
+
+function pattern8(n) {
+    for (let i = 0; i < n; i++) {
+        let star = "";
+        for (let j = 0; j <= i; j++) {
+            star += "*";
+        }
+        for (let j = 0; j < 2 * (n - i - 1); j++) {
+            star += ' ';
+        }
+        for (let j = 0; j <= i; j++) {
+            star += "*";
+        }
+        console.log(star);
+    }
+
+    for (let i = n - 2; i >= 0; i--) {
+        let star = '';
+        for (let j = 0; j <= i; j++) {
+            star += "*";
+        }
+        for (let j = 0; j < 2 * (n - i - 1); j++) {
+            star += ' ';
+        }
+        for (let j = 0; j <= i; j++) {
+            star += "*";
+        }
+        console.log(star);
+    }
+}
+
+pattern8(5);
+
+function pattern9(n){
+    for(let i=0; i<n; i++){
+        let star = '';
+        for(let j=0; j<n; j++){
+            if(i==0 || j==0 || i==n-1 || j== n-1){
+                star += "*";
+            }else{
+                star += ' ';
+            }
+        }
+        console.log(star);
+    }
+}
+pattern9(5);
